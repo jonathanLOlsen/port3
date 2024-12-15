9427c8effde8681c132c08f66576b9e6b20e1def
jonathanLOlsen <jonathan.laage.olsen@gmail.com>
2024-12-15 11:44:49 +0100

    rool to top

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Breadcrumbs from './Breadcrumbs';
+import ScrollTop from './ScrollTop'
 import './MainLayout.css'; // Optional if you want to style the layout
 
 const MainLayout = ({ children }) => {
   return (
     <div className="main-layout">
+      <ScrollTop/>
       <Header />
       <Breadcrumbs />
       <main className="main-content">
